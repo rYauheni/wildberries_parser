f4aff1b5163da5b0b6cb7c6e2682ef2fb364f0d9
rYauheni <ra.yauheni@gmail.com>
2024-04-03 13:41:44 +0300

    Refactor code

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
-from time import sleep
-
-from app_state_data.app_state_data_csv import CSVAppStateDataStorage
-from exceptions.exceptions import FileError
-from exceptions.error_messages import FILE_NOT_FOUND_MESSAGE
-from parser import get_messages
 from excel_utils import get_excel_file_path, extract_ids_from_excel
-from settings import APP_STATE_SERVICE
-from telegram_bot import send_message
+from exceptions.error_messages import FILE_NOT_FOUND_MESSAGE
+from exceptions.exceptions import FileError
+from notification_services.notification_messages import create_messages_list
+from objects.product import Product
+from parser import get_product_data
+from settings import APP_STATE_SERVICE, NOTIFICATION_SERVICE
+# from notification_services.telegram_notification_service import send_message
 
 
 def main():
     app_state_service = APP_STATE_SERVICE()
+    notification_service = NOTIFICATION_SERVICE()
     app_state_service.create_app_state_data()
     file_path = get_excel_file_path()
     try:
         products_ids = extract_ids_from_excel(file_path=file_path)
     except FileError:
-        send_message(FILE_NOT_FOUND_MESSAGE)
+        notification_service.send_message(message=FILE_NOT_FOUND_MESSAGE)
         return
     # last_update = app_state_service.set_default_last_update()
-    last_update = '2024-03-30T13:06:31Z'
+    last_update = '2024-03-31T13:06:31Z'
     for pid in products_ids:
+        product = Product(id=pid)
         app_state_service.set_app_state_data(pid, last_update)
+        product.last_update = app_state_service.get_app_state_data(pid=product.id)
+        get_product_data(product=product)
+        messages_list = create_messages_list(product=product)
 
-    for pid in products_ids:
-        messages = get_messages(pid=pid, app_state_service=app_state_service)
-        if messages:
-            for message in messages:
-                send_message(message=message)
-    print('====DONE====')
+        for message in messages_list:
+            notification_service.send_message(message=message)
+
+
+    # for pid in products_ids:
+    #     messages = get_messages(pid=pid, app_state_service=app_state_service)
+    #     if messages:
+    #         for message in messages:
+    #             send_message(message=message)
+    # print('====DONE====')
 
 
 if __name__ == '__main__':
--- parser.py
+++ parser.py
@@ -1,5 +1,9 @@
 import requests
 
+from exceptions.exceptions import RootError, FeedbackDataError
+from objects.feedback import Feedback
+from objects.product import Product
+
 
 def get_product_detail_url(pid: int) -> str:
     url = f'https://card.wb.ru/cards/detail?nm={pid}'
@@ -14,73 +18,99 @@ def get_product_feedback_urls(root: int) -> list:
 def parse_product_root(pid: int) -> int:
     product_detail_url = get_product_detail_url(pid=pid)
     product_detail = requests.get(product_detail_url)
-    if product_detail.status_code == 200:
-        root = product_detail.json()['data']['products'][0]['root']
-    else:
-        root = 0
+    if not product_detail.status_code == 200:
+        raise RootError
+    root = product_detail.json()['data']['products'][0]['root']
     return root
 
 
-def parse_feedback_count(pid: int) -> (int, None):
-    feedback_count = 0
-
-    root = parse_product_root(pid)
-    if not root:
-        return
-
-    product_feedback_urls = get_product_feedback_urls(root)
-    for url in product_feedback_urls:
-        product_feedback = requests.get(url)
-        if product_feedback.status_code == 200:
-            f_count = product_feedback.json()['feedbackCount']
-            feedback_count += f_count
-
-    return feedback_count
+# def parse_feedback_count(pid: int) -> (int, None):
+#     feedback_count = 0
+#
+#     root = parse_product_root(pid)
+#
+#     product_feedback_urls = get_product_feedback_urls(root)
+#     for url in product_feedback_urls:
+#         product_feedback = requests.get(url)
+#         if product_feedback.status_code == 200:
+#             f_count = product_feedback.json()['feedbackCount']
+#             feedback_count += f_count
+#
+#     return feedback_count
 
 
-def parse_product_data(pid: int) -> (int, None):
-    product_detail_url = get_product_detail_url(pid=pid)
+def parse_product_data(product):
+    product_detail_url = get_product_detail_url(pid=product.id)
     product_detail = requests.get(product_detail_url)
-    product_data = {}
     if product_detail.status_code == 200:
-        product = product_detail.json()
-        product_name = str(product['data']['products'][0]['brand']) + ' ' + str(product['data']['products'][0]['name'])
-        product_rating = product['data']['products'][0]['reviewRating']
-        product_data['product_name'] = product_name
-        product_data['product_rating'] = product_rating
-    return product_data
+        product_data = product_detail.json()
+        product.name = (str(product_data['data']['products'][0]['brand']) + ' ' +
+                        str(product_data['data']['products'][0]['name']))
+        product.rating = product_data['data']['products'][0]['reviewRating']
 
 
-def get_messages(pid: int, app_state_service) -> (list, None):
-    root = parse_product_root(pid)
-    last_update = app_state_service.get_app_state_data(pid)
-    if not root:
-        return
+# def get_messages(pid: int, app_state_service) -> (list, None):
+#     root = parse_product_root(pid)
+#     last_update = app_state_service.get_app_state_data(pid)
+#     if not root:
+#         return
+#
+#     product_feedback_urls = get_product_feedback_urls(root)
+#     for url in product_feedback_urls:
+#         try:
+#             product_feedback = requests.get(url)
+#             messages = []
+#             if product_feedback.status_code == 200:
+#                 feedbacks = product_feedback.json()['feedbacks']
+#                 new_feedbacks = [fb for fb in feedbacks if fb['createdDate'] >= last_update]
+#                 for feedback in new_feedbacks:
+#                     mark = feedback['productValuation']
+#                     last_data = feedback['createdDate']
+#                     if last_data > last_update:
+#                         app_state_service.update_app_state_data(pid=pid, last_update=last_data)
+#                     if 1 <= mark <= 4:
+#                         text = feedback['text']
+#                         product_data = parse_product_data(pid)
+#                         try:
+#                             product_name = product_data['product_name']
+#                             product_rating = product_data['product_rating']
+#                         except KeyError:
+#                             product_name, product_rating = None, None
+#                         msg = (f'Негативный отзыв/Товар: {product_name}/SKU: {pid}/Оценка: {mark}/Отзыв: {text}/'
+#                                f'Текущий рейтинг товара: {product_rating}')
+#                         messages.append(msg)
+#             return messages
+#         except TypeError:
+#             return
 
-    product_feedback_urls = get_product_feedback_urls(root)
+
+def get_product_data(product):
+    parse_product_data(product)
+    try:
+        product.root = parse_product_root(product.id)
+    except RootError:
+        pass
+
+    product_feedback_urls = get_product_feedback_urls(product.root)
     for url in product_feedback_urls:
         try:
-            product_feedback = requests.get(url)
-            messages = []
-            if product_feedback.status_code == 200:
-                feedbacks = product_feedback.json()['feedbacks']
-                new_feedbacks = [fb for fb in feedbacks if fb['createdDate'] >= last_update]
-                for feedback in new_feedbacks:
-                    mark = feedback['productValuation']
-                    last_data = feedback['createdDate']
-                    if last_data > last_update:
-                        app_state_service.update_app_state_data(pid=pid, last_update=last_data)
-                    if 1 <= mark <= 4:
-                        text = feedback['text']
-                        product_data = parse_product_data(pid)
-                        try:
-                            product_name = product_data['product_name']
-                            product_rating = product_data['product_rating']
-                        except KeyError:
-                            product_name, product_rating = None, None
-                        msg = (f'Негативный отзыв/Товар: {product_name}/SKU: {pid}/Оценка: {mark}/Отзыв: {text}/'
-                               f'Текущий рейтинг товара: {product_rating}')
-                        messages.append(msg)
-            return messages
-        except TypeError:
-            return
+            get_negative_feedback_data(product=product, url=url)
+        except FeedbackDataError:
+            pass
+
+
+def get_negative_feedback_data(product, url):
+    last_update = product.last_update
+    try:
+        product_feedbacks = requests.get(url).json()['feedbacks']
+        new_feedbacks = [fb for fb in product_feedbacks if fb['createdDate'] >= last_update]
+        for new_feedback in new_feedbacks:
+            feedback = Feedback()
+            feedback.mark = new_feedback['productValuation']
+            feedback.text = new_feedback['text']
+            feedback.date = new_feedback['createdDate']
+            if feedback.is_negative():
+                product.feedbacks.append(feedback)
+
+    except Exception:
+        raise FeedbackDataError
