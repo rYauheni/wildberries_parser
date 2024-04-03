40094da00dddd5487cd9ee689b39ccb729a6bffb
rYauheni <ra.yauheni@gmail.com>
2024-04-03 13:38:14 +0300

    Create custom exceptions for invalid product root and invalid product feedback.

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -11,3 +11,33 @@ class FileError(Exception):
             return f'FileError:  No matching file found. Detail: {self.message}.'
         else:
             return 'FileError:  No matching file found.'
+
+
+class RootError(ValueError):
+
+    def __init__(self, *args):
+        if args:
+            self.message = args
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return f'RootError: Product root not found. Detail: {self.message}.'
+        else:
+            return 'RootError:  Product root not found.'
+
+
+class FeedbackDataError(Exception):
+
+    def __init__(self, *args):
+        if args:
+            self.message = args
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return f'FeedbackDataError: Feedback not found. Detail: {self.message}.'
+        else:
+            return 'FeedbackDataError:  Feedback not found.'
