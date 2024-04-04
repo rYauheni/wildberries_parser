514a52bfd5919d728e30670e27e0323822ee087d
rYauheni <ra.yauheni@gmail.com>
2024-04-04 19:08:53 +0300

    Decompose main func in main.py.

--- main.py
+++ main.py
@@ -7,22 +7,67 @@ from exceptions.error_messages import (FILE_NOT_FOUND_MESSAGE,
                                        FEEDBACK_DATA_NOT_FOUND_MESSAGE,
                                        EXCEPTION_MESSAGE, )
 from exceptions.exceptions import FileError, RootError, ProductDataError, FeedbackDataError, NotificationError
-from notification_services.notification_messages import (create_messages_list,
+from notification_services.notification_messages import (fill_messages_list,
                                                          create_not_found_negative_feedbacks_message)
 from objects.product import Product
 from parser import get_product_data
 from settings import APP_STATE_SERVICE, NOTIFICATION_SERVICE
-
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
+app_state_service = APP_STATE_SERVICE()
+
+notification_service = NOTIFICATION_SERVICE()
+
+
+def send_message(message: str, pid: int):
+    try:
+        notification_service.send_message(message=message)
+        logger.info(f'Product {pid}. Message was sent successfully.')
+    except NotificationError:
+        logger.info(f'ERROR. Product {pid}. Message did not be sent.')
+
+
+def send_messages(messages_list: list, product: Product):
+    if messages_list:
+        for message in messages_list:
+            send_message(message=message, pid=product.id)
+
+    else:
+        message = create_not_found_negative_feedbacks_message(product=product)
+        send_message(message, pid=product.id)
+
+
+def create_messages_list(product) -> list:
+    messages_list = []
+    try:
+
+        product.last_update = app_state_service.get_app_state_data(pid=product.id)
+        get_product_data(product=product)
+        messages_list = fill_messages_list(product=product, messages_list=messages_list)
+
+        app_state_service.update_app_state_data(pid=product.id, last_update=product.last_update)
+        logger.info(f'Product {product.id} has been parsed.')
+    except RootError:
+        notification_service.send_message(message=ROOT_NOT_FOUND_MESSAGE)
+        logger.info(f'ERROR. Product {product.id} root not found or could not be parsed.')
+    except ProductDataError:
+        notification_service.send_message(message=PRODUCT_DATA_NOT_FOUND_MESSAGE)
+        logger.info(f'ERROR. Product {product.id} data not found or could not be parsed.')
+    except FeedbackDataError:
+        notification_service.send_message(message=FEEDBACK_DATA_NOT_FOUND_MESSAGE)
+        logger.info(f'ERROR. Product {product.id} feedback data not found or could not be parsed.')
+    except Exception as e:
+        notification_service.send_message(message=EXCEPTION_MESSAGE)
+        logger.info(f'ERROR. Product {product.id} raised exception {e}.')
+
+    return messages_list
+
 
 def main():
-    app_state_service = APP_STATE_SERVICE()
-    notification_service = NOTIFICATION_SERVICE()
     app_state_service.create_app_state_data()
     file_path = get_excel_file_path()
     try:
@@ -35,48 +80,13 @@ def main():
     # last_update = app_state_service.set_default_last_update()
     default_last_update = '2024-03-31T13:06:31Z'
     for pid in products_ids:
-        try:
-            product = Product(id=pid)
-            if not app_state_service.get_app_state_data(pid=product.id):
-                app_state_service.set_app_state_data(pid, default_last_update)
-            product.last_update = app_state_service.get_app_state_data(pid=product.id)
-            get_product_data(product=product)
-            messages_list = create_messages_list(product=product)
+        product = Product(id=pid)
+        if not app_state_service.get_app_state_data(pid=product.id):
+            app_state_service.set_app_state_data(pid=product.id, last_update=default_last_update)
 
-            app_state_service.update_app_state_data(pid=product.id, last_update=product.last_update)
-            logger.info(f'Product {pid} has been parsed.')
-        except RootError:
-            notification_service.send_message(message=ROOT_NOT_FOUND_MESSAGE)
-            logger.info(f'ERROR. Product {pid} root not found or could not be parsed.')
-            continue
-        except ProductDataError:
-            notification_service.send_message(message=PRODUCT_DATA_NOT_FOUND_MESSAGE)
-            logger.info(f'ERROR. Product {pid} data not found or could not be parsed.')
-            continue
-        except FeedbackDataError:
-            notification_service.send_message(message=FEEDBACK_DATA_NOT_FOUND_MESSAGE)
-            logger.info(f'ERROR. Product {pid} feedback data not found or could not be parsed.')
-            continue
-        except Exception as e:
-            notification_service.send_message(message=EXCEPTION_MESSAGE)
-            logger.info(f'ERROR. Product {pid} raised exception {e}.')
-            continue
+        messages_list = create_messages_list(product=product)
 
-        if messages_list:
-            for message in messages_list:
-                try:
-                    notification_service.send_message(message=message)
-                    logger.info(f'Product {pid}. Message was sent successfully.')
-                except NotificationError:
-                    logger.info(f'ERROR. Product {pid}. Message did not be sent.')
-                    continue
-        else:
-            try:
-                message = create_not_found_negative_feedbacks_message(product=product)
-                notification_service.send_message(message=message)
-                logger.info(f'Product {pid}. Message was sent successfully.')
-            except NotificationError:
-                logger.info(f'ERROR. Product {pid}. Message did not be sent.')
+        send_messages(messages_list=messages_list, product=product)
 
 
 if __name__ == '__main__':
--- notification_services/notification_messages.py
+++ notification_services/notification_messages.py
@@ -1,8 +1,7 @@
 from objects.product import Product
 
 
-def create_messages_list(product: Product) -> list:
-    messages_list = []
+def fill_messages_list(product: Product, messages_list: list) -> list:
     if product.feedbacks:
         for feedback in product.feedbacks:
             message = (f'New negative review.\n'
