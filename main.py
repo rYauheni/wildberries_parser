57e03132a42dedc0fcc58e372ce08a03d0e155cc
rYauheni <ra.yauheni@gmail.com>
2024-04-06 17:59:09 +0300

    Services and Managers pass in function as arguments.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 
-from excel_utils import get_excel_file_path, extract_ids_from_excel
+from app_state_data_service.json_app_state_data_service import JSONAppStateDataService
 from exceptions.error_messages import (FILE_NOT_FOUND_MESSAGE,
                                        IDS_NOT_FOUND,
                                        PRODUCT_DATA_NOT_FOUND_MESSAGE,
@@ -10,27 +10,18 @@ from exceptions.exceptions import FileError, ProductDataError, FeedbackDataError
 from notification_services.notification_manager import NotificationManager
 from notification_services.notification_messages import (fill_messages_list,
                                                          create_not_found_negative_feedbacks_message)
+from notification_services.telegram_notification_service import TelegramNotificationService
 from models.product import Product
 from parser import get_product_data
 from product_id_access_services.excel_product_id_access_service import ExcelProductIDAccessService
-from settings import app_state_service, notification_services_list
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
-app_state_service = app_state_service()
 
-notification_services = [service() for service in notification_services_list]
-
-notification_manager = NotificationManager()
-notification_manager.add_services(services=notification_services)
-
-product_id_access_service = ExcelProductIDAccessService()
-
-
-def send_message(message: str, pid: int):
+def send_message(message: str, pid: int, notification_manager: NotificationManager):
     try:
         notification_manager.send_message(message=message)
         logger.info(f'Product {pid}. Message was sent successfully.')
@@ -38,17 +29,17 @@ def send_message(message: str, pid: int):
         logger.info(f'ERROR. Product {pid}. Message did not be sent.')
 
 
-def send_messages(messages_list: list, product: Product):
+def send_messages(messages_list: list, product: Product, notification_manager: NotificationManager):
     if messages_list:
         for message in messages_list:
-            send_message(message=message, pid=product.id)
+            send_message(message=message, pid=product.id, notification_manager=notification_manager)
 
     else:
         message = create_not_found_negative_feedbacks_message(product=product)
-        send_message(message, pid=product.id)
+        send_message(message, pid=product.id, notification_manager=notification_manager)
 
 
-def create_messages_list(product) -> list:
+def create_messages_list(product: Product, notification_manager: NotificationManager) -> list:
     messages_list = []
     try:
         get_product_data(product=product)
@@ -67,24 +58,33 @@ def create_messages_list(product) -> list:
     return messages_list
 
 
-def get_products_ids() -> list:
+def get_products_ids(prodict_service: ExcelProductIDAccessService, notification_manager: NotificationManager) -> list:
     products_ids = []
     try:
-        products_ids = product_id_access_service.get_ids()
-        logger.info(f'Extract ids from {product_id_access_service.source}.')
+        products_ids = prodict_service.get_ids()
+        logger.info(f'Extract ids from {prodict_service.source}.')
     except FileError:
         notification_manager.send_message(message=FILE_NOT_FOUND_MESSAGE)
-        logger.info(f'ERROR. File {product_id_access_service.source} not found or the file could not be parsed.')
+        logger.info(f'ERROR. File {prodict_service.source} not found or the file could not be parsed.')
 
     if not products_ids:
         notification_manager.send_message(message=IDS_NOT_FOUND)
-        logger.info(f'No ids found in the file {product_id_access_service.source}.')
+        logger.info(f'No ids found in the file {prodict_service.source}.')
     return products_ids
 
 
 def main():
+    app_state_service = JSONAppStateDataService()
+
+    notification_services = (TelegramNotificationService(), )
+    notification_manager = NotificationManager()
+    notification_manager.add_services(services=notification_services)
+
+    product_id_access_service = ExcelProductIDAccessService()
+
     app_state_service.create_app_state_data()
-    products_ids = get_products_ids()
+    products_ids = get_products_ids(prodict_service=product_id_access_service,
+                                    notification_manager=notification_manager)
     # default_last_update = app_state_service.set_default_last_update()
     default_last_update = '2024-04-02T13:06:31Z'
     for pid in products_ids:
@@ -92,11 +92,11 @@ def main():
         if not app_state_service.get_app_state_data(pid=product.id):
             app_state_service.set_app_state_data(pid=product.id, last_update=default_last_update)
         product.last_update = app_state_service.get_app_state_data(pid=product.id)
-        messages_list = create_messages_list(product=product)
+        messages_list = create_messages_list(product=product, notification_manager=notification_manager)
         app_state_service.update_app_state_data(pid=product.id,
                                                 last_update=product.last_update)  # WARNING! Update state data must be after create_messages_list()
 
-        send_messages(messages_list=messages_list, product=product)
+        send_messages(messages_list=messages_list, product=product, notification_manager=notification_manager)
 
 
 if __name__ == '__main__':
