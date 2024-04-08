255081e6da521c1ed5e222b04549df6d750196a5
rYauheni <ra.yauheni@gmail.com>
2024-04-08 16:48:43 +0300

    Functions send_message, send_messages, create_messages_list move from main.py to separate module notification_utils.py.

--- main.py
+++ main.py
@@ -1,62 +1,10 @@
-import logging
-
 from app_state_data_service.json_app_state_data_service import JSONAppStateDataService
-from exceptions.error_messages import (FILE_NOT_FOUND_MESSAGE,
-                                       IDS_NOT_FOUND,
-                                       PRODUCT_DATA_NOT_FOUND_MESSAGE,
-                                       FEEDBACK_DATA_NOT_FOUND_MESSAGE,
-                                       EXCEPTION_MESSAGE, )
-from exceptions.exceptions import FileError, ProductDataError, FeedbackDataError, NotificationError
 from notification_services.notification_manager import NotificationManager
-from notification_services.notification_messages import (fill_messages_list,
-                                                         create_not_found_negative_feedbacks_message)
+from notification_services.notification_utils import create_messages_list, send_messages
 from notification_services.telegram_notification_service import TelegramNotificationService
 from models.product import Product
-from parser import get_product_data
 from product_id_access_services.excel_product_id_access_service import ExcelProductIDAccessService
 from product_id_access_services.product_id_access_utils import get_products_ids
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-logger = logging.getLogger(__name__)
-
-
-def send_message(message: str, pid: int, notification_manager: NotificationManager):
-    try:
-        notification_manager.send_message(message=message)
-        logger.info(f'Product {pid}. Message was sent successfully.')
-    except NotificationError:
-        logger.error(f'ERROR. Product {pid}. Message did not be sent.')
-
-
-def send_messages(messages_list: list, product: Product, notification_manager: NotificationManager):
-    if messages_list:
-        for message in messages_list:
-            send_message(message=message, pid=product.id, notification_manager=notification_manager)
-
-    else:
-        message = create_not_found_negative_feedbacks_message(product=product)
-        send_message(message, pid=product.id, notification_manager=notification_manager)
-
-
-def create_messages_list(product: Product, notification_manager: NotificationManager) -> list:
-    messages_list = []
-    try:
-        get_product_data(product=product)
-        messages_list = fill_messages_list(product=product, messages_list=messages_list)
-        logger.info(f'Product {product.id} has been parsed.')
-    except ProductDataError:
-        notification_manager.send_message(message=PRODUCT_DATA_NOT_FOUND_MESSAGE)
-        logger.error(f'ERROR. Product {product.id} data not found or could not be parsed.')
-    except FeedbackDataError:
-        notification_manager.send_message(message=FEEDBACK_DATA_NOT_FOUND_MESSAGE)
-        logger.error(f'ERROR. Product {product.id} feedback data not found or could not be parsed.')
-    except Exception as e:
-        notification_manager.send_message(message=EXCEPTION_MESSAGE)
-        logger.error(f'ERROR. Product {product.id} raised exception {e}.')
-
-    return messages_list
 
 
 def main():
---
+++ notification_services/notification_utils.py
@@ -0,0 +1,50 @@
+import logging
+
+from exceptions.error_messages import PRODUCT_DATA_NOT_FOUND_MESSAGE, FEEDBACK_DATA_NOT_FOUND_MESSAGE, EXCEPTION_MESSAGE
+from exceptions.exceptions import NotificationError, ProductDataError, FeedbackDataError
+from models.product import Product
+from notification_services.notification_manager import NotificationManager
+from notification_services.notification_messages import create_not_found_negative_feedbacks_message, fill_messages_list
+from parser import get_product_data
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+def send_message(message: str, pid: int, notification_manager: NotificationManager):
+    try:
+        notification_manager.send_message(message=message)
+        logger.info(f'Product {pid}. Message was sent successfully.')
+    except NotificationError:
+        logger.error(f'ERROR. Product {pid}. Message did not be sent.')
+
+
+def send_messages(messages_list: list, product: Product, notification_manager: NotificationManager):
+    if messages_list:
+        for message in messages_list:
+            send_message(message=message, pid=product.id, notification_manager=notification_manager)
+
+    else:
+        message = create_not_found_negative_feedbacks_message(product=product)
+        send_message(message, pid=product.id, notification_manager=notification_manager)
+
+
+def create_messages_list(product: Product, notification_manager: NotificationManager) -> list:
+    messages_list = []
+    try:
+        get_product_data(product=product)
+        messages_list = fill_messages_list(product=product, messages_list=messages_list)
+        logger.info(f'Product {product.id} has been parsed.')
+    except ProductDataError:
+        notification_manager.send_message(message=PRODUCT_DATA_NOT_FOUND_MESSAGE)
+        logger.error(f'ERROR. Product {product.id} data not found or could not be parsed.')
+    except FeedbackDataError:
+        notification_manager.send_message(message=FEEDBACK_DATA_NOT_FOUND_MESSAGE)
+        logger.error(f'ERROR. Product {product.id} feedback data not found or could not be parsed.')
+    except Exception as e:
+        notification_manager.send_message(message=EXCEPTION_MESSAGE)
+        logger.error(f'ERROR. Product {product.id} raised exception {e}.')
+
+    return messages_list
