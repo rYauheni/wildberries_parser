591a243a3196c0dcb0884c00f0126c15ee5220dd
rYauheni <ra.yauheni@gmail.com>
2024-04-09 14:37:52 +0300

    Add comments for code.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from app_state_data_service.json_app_state_data_service import JSONAppStateDataService
+from models.product import Product
 from notification_services.notification_manager import NotificationManager
 from notification_services.notification_utils import create_messages_list, send_messages
 from notification_services.telegram_notification_service import TelegramNotificationService
-from models.product import Product
 from product_id_access_services.excel_product_id_access_service import ExcelProductIDAccessService
 from product_id_access_services.product_id_access_utils import get_products_ids
 
@@ -19,8 +19,8 @@ def main():
     app_state_service.create_app_state_data()
     products_ids = get_products_ids(prodict_service=product_id_access_service,
                                     notification_manager=notification_manager)
-    # default_last_update = app_state_service.set_default_last_update()
-    default_last_update = '2024-04-04T13:06:31Z'
+    # default_last_update = app_state_service.set_default_last_update()  # Enable for prod.
+    default_last_update = '2024-04-04T13:06:31Z'  # Enable for dev only.
     for pid in products_ids:
         product = Product(id=pid)
         if not app_state_service.get_app_state_data(pid=product.id):
