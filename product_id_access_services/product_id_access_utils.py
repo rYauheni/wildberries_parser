306db5a05b722e557a3eef2f641588631169a5bd
rYauheni <ra.yauheni@gmail.com>
2024-04-12 15:26:55 +0300

    Add handler for retries requests to TelegramAPI.

--- product_id_access_services/product_id_access_utils.py
+++ product_id_access_services/product_id_access_utils.py
@@ -1,16 +1,8 @@
-import logging
-
 from exceptions.error_messages import IDS_NOT_FOUND, FILE_NOT_FOUND_MESSAGE
 from exceptions.exceptions import FileError
+from logger_utils.logger_utils import logger
 from notification_services.notification_manager import NotificationManager
 from product_id_access_services.excel_product_id_access_service import ExcelProductIDAccessService
-
-# logging.basicConfig(
-#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-# )
-# logger = logging.getLogger(__name__)
-
-from logger_utils.logger_utils import logger
 
 
 def get_products_ids(prodict_service: ExcelProductIDAccessService, notification_manager: NotificationManager) -> list:
