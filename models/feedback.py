08d23ad2c431d3affa1ddf3489025aab103b6cb1
rYauheni <ra.yauheni@gmail.com>
2024-04-18 18:06:19 +0300

    Add errors handler.

--- models/feedback.py
+++ models/feedback.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+from exceptions.exceptions import FeedbackDataError
+
 
 @dataclass
 class Feedback:
@@ -8,9 +10,12 @@ class Feedback:
     date: str = None
 
     def get_feedback_data_from_json(self, feedback_detail):
-        self.mark = feedback_detail['productValuation']
-        self.text = feedback_detail['text']
-        self.date = feedback_detail['createdDate']
+        try:
+            self.mark = feedback_detail['productValuation']
+            self.text = feedback_detail['text']
+            self.date = feedback_detail['createdDate']
+        except KeyError:
+            raise FeedbackDataError
 
     def is_new(self, last_update):
         if self.date > last_update:
