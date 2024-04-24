1aaf13c84c85e3dd09d115b4e3cb9b010a442323
rYauheni <ra.yauheni@gmail.com>
2024-04-24 12:36:08 +0300

    Get file with ids path replace to separate class FileProvider in separate module file_provider.py.

--- product_id_extract_services/file_provider.py
+++ product_id_extract_services/file_provider.py
@@ -7,9 +7,9 @@ class FileProvider:
     @staticmethod
     def get_file_path() -> str:
         args = sys.argv
-        if '--ids_source' not in args:
+        if '--ids-file_path' not in args:
             excel_file_path = DEFAULT_EXCEL_FILE_PATH
         else:
-            excel_file_path_index = args.index("--ids_source") + 1
+            excel_file_path_index = args.index("--ids-file_path") + 1
             excel_file_path = args[excel_file_path_index]
         return excel_file_path
