7ac9069fcccc1721274d60bcef70b4abf7f53a52
rYauheni <ra.yauheni@gmail.com>
2024-04-24 12:32:56 +0300

    Get file with ids path replace to separate class FileProvider in separate module file_provider.py.

--- product_id_extract_services/excel_product_id_extract_service.py
+++ product_id_extract_services/excel_product_id_extract_service.py
@@ -1,28 +1,21 @@
-import sys
-
 import openpyxl
 
 from exceptions.exceptions import SourceError
 from logger_utils.logger_utils import logger
+from product_id_extract_services.file_provider import FileProvider
 from product_id_extract_services.product_id_extract_service import ProductIDExtractService
-from settings import DEFAULT_EXCEL_FILE_PATH
 
 
 class ExcelProductIDExtractService(ProductIDExtractService):
     ids: list[int]
 
-    @staticmethod
-    def get_excel_file_path() -> str:
-        args = sys.argv
-        if '--ids_source' not in args:
-            excel_file_path = DEFAULT_EXCEL_FILE_PATH
-        else:
-            excel_file_path_index = args.index("--ids_source") + 1
-            excel_file_path = args[excel_file_path_index]
-        return excel_file_path
-
     def get_ids(self) -> list:
-        file_path = self.get_excel_file_path()
+
+        # You cannot pass file_path as an argument to this method,
+        # because not every ProductIDExtractService (for example service for database)
+        # will have the file_path parameter.
+        # Compliance with the LSP.
+        file_path = FileProvider().get_file_path()
         logger.info(f'ID source is excel file: {file_path}')
         self.ids = []
         try:
---
+++ product_id_extract_services/file_provider.py
@@ -0,0 +1,15 @@
+import sys
+
+from settings import DEFAULT_EXCEL_FILE_PATH
+
+
+class FileProvider:
+    @staticmethod
+    def get_file_path() -> str:
+        args = sys.argv
+        if '--ids_source' not in args:
+            excel_file_path = DEFAULT_EXCEL_FILE_PATH
+        else:
+            excel_file_path_index = args.index("--ids_source") + 1
+            excel_file_path = args[excel_file_path_index]
+        return excel_file_path
