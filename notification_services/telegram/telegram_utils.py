1ff612bc2ae8fecd761944c60f4b942b45344d20
rYauheni <ra.yauheni@gmail.com>
2024-04-25 18:09:00 +0300

    Telegram send message logic moved from TelegramNotificationService class telegram_send_message func.

--- notification_services/telegram/telegram_utils.py
+++ notification_services/telegram/telegram_utils.py
@@ -1,10 +1,15 @@
+import os
 import time
 from functools import wraps
 
+from dotenv import load_dotenv
 import requests
 
 from exceptions.exceptions import NotificationError
 from logger_utils.logger_utils import logger
+
+
+load_dotenv()
 
 
 def handle_rate_limit(retries: int = 3, delay: int = 40):
@@ -32,7 +37,9 @@ def handle_rate_limit(retries: int = 3, delay: int = 40):
 
 
 @handle_rate_limit(retries=3, delay=40)
-def telegram_send_message(token: str, chat_id: str, message: str):
+def telegram_send_message(message: str):
+    token = os.environ.get('TOKEN')
+    chat_id = os.environ.get('CHAT_ID')
     try:
         url = f'https://api.telegram.org/bot{token}/sendMessage'
         params = {'chat_id': chat_id, 'text': message}
