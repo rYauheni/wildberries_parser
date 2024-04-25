b42ebb95100b174b048995a4665ea90929b5b200
rYauheni <ra.yauheni@gmail.com>
2024-04-25 14:32:13 +0300

    Create product object logic move to product_parser.py.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from models.message import MessagesList
 from models.product import Product, Status
 from notification_services.notification_manager import NotificationManager
 from notification_services.telegram_notification_service import TelegramNotificationService
-from parser.product_parser import ProductParser
+from parser.product_parser import create_product
 from product_id_extract_services.excel_product_id_extract_service import ExcelProductIDExtractService
 from product_id_extract_services.file_provider import FileProvider
 from product_id_extract_services.product_id_extract_utils import get_products_ids
@@ -28,23 +28,11 @@ def main():
             app_state_service.set_product_data(pid=pid, last_update=default_last_update)
         last_update = float(app_state_service.get_product_data(pid=pid))
 
-
-
-        product_parser = ProductParser(pid=pid, last_update=last_update)
-        product = Product(
-            id=pid,
-            url=product_parser.product_url,
-            root=product_parser.product_root_from_json(),
-            name=product_parser.product_name_from_json(),
-            rating=product_parser.product_rating_from_json(),
-            feedbacks=product_parser.parse_product_feedbacks(),
-            last_update=product_parser.product_last_update # last_update must necessarily come after feedbacks, otherwise there is a violation of logic and incorrect data
-        )
-
-        # product.handle_product_data_parser() TODO
+        product = create_product(pid=pid, product_last_update=last_update)
 
         messages_list = MessagesList(product=product)
         messages_list.fill_messages_list()
+        # TODO: SRP disturb: update app state does not be in this method
         messages_list.handle_messages_sender(notification_manager=notification_manager,
                                              app_state_service=app_state_service)
 
--- parser/product_parser.py
+++ parser/product_parser.py
@@ -1,120 +1,130 @@
 import requests
 
-from exceptions.exceptions import ProductDataError, FeedbackDataError
+from exceptions.exceptions import ProductDataError, FeedbackDataError, RootError
+from logger_utils.logger_utils import logger
 from models.feedback import Feedback
+from models.product import Product, Status
 
 
-class ProductParser:
-    def __init__(self, pid: int, last_update: float):
-        self.product_last_update = last_update
-        self.product_url = f'https://card.wb.ru/cards/detail?nm={pid}'
-        self.product_data = self.parse_product_data()
+def create_product(pid: int, product_last_update: float):
+    product_url = f'https://card.wb.ru/cards/detail?nm={pid}'
+    product_status = Status.GOOD
+    product_root = None
+    product_name = None
+    product_rating = None
+    product_last_update = product_last_update
+    product_feedbacks = None
 
-    # def get_product_data_from_json(self, product_detail):
-    #     data = product_detail.json()
-    #     try:
-    #         self.root = data['data']['products'][0]['root']
-    #
-    #         self.name = (str(data['data']['products'][0]['brand']) + ' ' +
-    #                      str(data['data']['products'][0]['name']))
-    #         self.rating = data['data']['products'][0]['reviewRating']
-    #
-    #     except KeyError:
-    #         raise ProductDataError
-
-    def parse_product_data(self):
+    try:
         try:
-            product_detail = requests.get(self.product_url)
-        except Exception:
-            raise ProductDataError
-        return product_detail.json()
+            product_detail = parse_product_data(product_url=product_url)
+            product_root = product_root_from_json(product_detail=product_detail)
+            product_name = product_name_from_json(product_detail=product_detail)
+            product_rating = product_rating_from_json(product_detail=product_detail)
+            logger.info(f'Product {pid} has been parsed.')
+        except ProductDataError:
+            product_status = Status.PRODUCT_DNF
+            logger.error(f'Product {pid} data not found or could not be parsed.')
 
-    def product_root_from_json(self):
-        try:
-            product_root = self.product_data['data']['products'][0]['root']
-        except KeyError:
-            raise ProductDataError
-        return product_root
+        feedbacks_urls = get_feedbacks_urls(product_root=product_root)
 
-    def product_name_from_json(self):
-        try:
-            product_name = (str(self.product_data['data']['products'][0]['brand']) + ' ' +
-                            str(self.product_data['data']['products'][0]['name']))
-        except KeyError:
-            raise ProductDataError
-        return product_name
+        if product_status == Status.GOOD:
+            try:
+                product_feedbacks = parse_product_feedbacks(feedbacks_urls=feedbacks_urls,
+                                                            product_last_update=product_last_update)
+                logger.info(f'Product {pid} feedbacks has been parsed.')
+            except FeedbackDataError:
+                product_status = Status.FEEDBACK_DNF
+                logger.error(f'Product {pid} feedbacks could not be parsed.')
 
-    def product_rating_from_json(self):
-        try:
-            product_rating = self.product_data['data']['products'][0]['reviewRating']
-        except KeyError:
-            raise ProductDataError
-        return product_rating
+        if product_status == Status.GOOD:
+            product_last_update = update_product_last_update(n_feedbacks=product_feedbacks,
+                                                             product_last_update=product_last_update)
+    except Exception as e:
+        product_status = Status.UNKNOWN_E
+        logger.error(f'Product {pid} raised exception {e}.')
 
-    # def get_product_detail_url(self):
-    #     self.url = f'https://card.wb.ru/cards/detail?nm={self.id}'
+    product = Product(
+        id=pid,
+        status=product_status,
+        url=product_url,
+        root=product_root,
+        name=product_name,
+        rating=product_rating,
+        last_update=product_last_update,
+        feedbacks=product_feedbacks
+    )
 
-    def parse_product_feedbacks(self):
-        feedbacks_urls = self.get_feedbacks_urls()
-        try:
-            negative_feedbacks = self.get_negative_feedbacks(feedbacks_urls=feedbacks_urls)
-        except Exception:
-            raise FeedbackDataError
-        return negative_feedbacks
+    return product
 
-    def get_feedbacks_urls(self) -> list[str]:
-        product_root = self.product_root_from_json()
-        feedbacks_urls = [f'https://feedbacks{i}.wb.ru/feedbacks/v1/{product_root}' for i in range(1, 3)]
-        return feedbacks_urls
 
-    def get_negative_feedbacks(self, feedbacks_urls):
-        negative_feedbacks = []
-        try:
-            for url in feedbacks_urls:
-                product_feedbacks = requests.get(url).json()['feedbacks']
-                if not product_feedbacks:
-                    continue
+def parse_product_data(product_url: str):
+    try:
+        product_detail = requests.get(product_url)
+    except Exception:
+        raise ProductDataError
+    return product_detail.json()
 
-                new_update = self.product_last_update
-                for pf in product_feedbacks:
-                    feedback = Feedback()
-                    feedback.get_feedback_data_from_json(feedback_detail=pf)
-                    if feedback.is_negative() and feedback.is_new(last_update=self.product_last_update):
-                        negative_feedbacks.append(feedback)
-                        if feedback.date > new_update:
-                            new_update = feedback.date
 
-                self.product_last_update = new_update
+def product_root_from_json(product_detail):
+    try:
+        product_root = product_detail['data']['products'][0]['root']
+    except KeyError:
+        raise RootError
+    return product_root
 
-                return negative_feedbacks
 
-        except Exception:
-            raise FeedbackDataError
+def product_name_from_json(product_detail):
+    try:
+        product_name = (str(product_detail['data']['products'][0]['brand']) + ' ' +
+                        str(product_detail['data']['products'][0]['name']))
+    except KeyError:
+        raise ProductDataError
+    return product_name
 
-    # def parse_product_data(self):
-    #     try:
-    #         product_detail = requests.get(self.product_url)
-    #     except Exception:
-    #         raise ProductDataError
-    #     return product_detail
 
-        # self.get_product_data_from_json(product_detail=product_detail)
-        # feedbacks_urls = self.get_feedbacks_urls()
-        # try:
-        #     self.get_negative_feedbacks(feedbacks_urls=feedbacks_urls)
-        # except Exception:
-        #     raise FeedbackDataError
+def product_rating_from_json(product_detail):
+    try:
+        product_rating = product_detail['data']['products'][0]['reviewRating']
+    except KeyError:
+        raise ProductDataError
+    return product_rating
 
-    # def handle_product_data_parser(self):
-    #     try:
-    #         self.parse_product_data()
-    #         logger.info(f'Product {self.id} has been parsed.')
-    #     except ProductDataError:
-    #         self.status = Status.PRODUCT_DNF
-    #         logger.error(f'Product {self.id} data not found or could not be parsed.')
-    #     except FeedbackDataError:
-    #         self.status = Status.FEEDBACK_DNF
-    #         logger.error(f'Product {self.id} feedback data not found or could not be parsed.')
-    #     except Exception as e:
-    #         self.status = Status.UNKNOWN_E
-    #         logger.error(f'Product {self.id} raised exception {e}.')
+
+def parse_product_feedbacks(feedbacks_urls, product_last_update):
+    try:
+        negative_feedbacks = get_negative_feedbacks(feedbacks_urls=feedbacks_urls,
+                                                    product_last_update=product_last_update)
+    except Exception:
+        raise FeedbackDataError
+    return negative_feedbacks
+
+
+def get_feedbacks_urls(product_root) -> list[str]:
+    feedbacks_urls = [f'https://feedbacks{i}.wb.ru/feedbacks/v1/{product_root}' for i in range(1, 3)]
+    return feedbacks_urls
+
+
+def get_negative_feedbacks(feedbacks_urls, product_last_update):
+    negative_feedbacks = []
+    try:
+        for url in feedbacks_urls:
+            product_feedbacks = requests.get(url).json()['feedbacks']
+            if not product_feedbacks:
+                continue
+            for pf in product_feedbacks:
+                feedback = Feedback()
+                feedback.get_feedback_data_from_json(feedback_detail=pf)
+                if feedback.is_negative() and feedback.is_new(last_update=product_last_update):
+                    negative_feedbacks.append(feedback)
+            return negative_feedbacks
+    except Exception:
+        raise FeedbackDataError
+
+
+def update_product_last_update(n_feedbacks, product_last_update):
+    new_update = product_last_update
+    for n_feedback in n_feedbacks:
+        if n_feedback.date > new_update:
+            new_update = n_feedback.date
+    return new_update
